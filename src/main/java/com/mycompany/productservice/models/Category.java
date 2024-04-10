1f4dc777d3a92e7e0f269bf081f85b9af1f474eb
Dineshkumarr-A <dinesh.athiappan@outlook.com>
2024-04-10 07:23:49 +0530

    added cascade types

--- src/main/java/com/mycompany/productservice/models/Category.java
+++ src/main/java/com/mycompany/productservice/models/Category.java
@@ -1,12 +1,18 @@
 package com.mycompany.productservice.models;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.List;
 
 @Getter
 @Setter
 @Entity
 public class Category extends BaseModel {
+    @OneToMany(mappedBy = "category", cascade = CascadeType.REMOVE)
+    private List<Product> products;
     private String title;
 }
